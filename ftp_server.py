97f7ad99ca1dc52d7225f1fcf6765d81a932aeb8
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2019-02-28 20:05:50 -0400

    Files have been altered to match PEP8

--- ftp_server.py
+++ ftp_server.py
@@ -6,38 +6,43 @@ from pyftpdlib.authorizers import DummyAuthorizer
 from pyftpdlib.handlers import FTPHandler
 from pyftpdlib.servers import ThreadedFTPServer
 
+
 class FTPServer:
 
-	server = None
+    server = None
 
+    def __init__(self, user):
 
-	def __init__(self, user):
+        # Instantiates a 'virtual' users manager
+        authorizer = DummyAuthorizer()
 
-		# Instantiates a 'virtual' users manager
-		authorizer = DummyAuthorizer()
+        # Adds a new user according to the following parameters:
+        # username, password, homedir, perm="", msg_login="", msg_quit=""
+        authorizer.add_user(
+            user.username,
+            user.password,
+            user.dir_,
+            perm=user.permissions)
 
-		# Adds a new user according to the following parameters:
-		# username, password, homedir, perm="", msg_login="", msg_quit=""
-		authorizer.add_user(user.username, user.password, user.dir_, perm=user.permissions)
+        # Instantiates the FTPHandler class
+        handler = FTPHandler
+        handler.authorizer = authorizer
 
-		# Instantiates the FTPHandler class
-		handler = FTPHandler
-		handler.authorizer = authorizer
+        # Defines a banner (string) that shows when user connects
+        handler.banner = 'Welcome to a Python FTP Server!'
 
-		# Defines a banner (string) that shows when user connects
-		handler.banner = 'Welcome to a Python FTP Server!'
+        # Instantiates the server class and listen on address [Machine
+        # IPv4]:2121
+        address = (str(ip_retriever.get_ip_address()), 2121)
+        self.server = ThreadedFTPServer(address, handler)
 
-		# Instantiates the server class and listen on address [Machine IPv4]:2121
-		address = (str(ip_retriever.get_ip_address()), 2121)
-		self.server = ThreadedFTPServer(address, handler)
+        # Limits the amount of possible connections
+        self.server.max_cons = 10
+        self.server.max_cons_per_ip = 5
 
-		# Limits the amount of possible connections
-		self.server.max_cons = 10
-		self.server.max_cons_per_ip = 5
-		
-		print('\nServer Address: ftp://{}:{}\n'.format(address[0], address[1]))
-		
-	def start_server(self):
-		
-		# Launch FTP Server
-		self.server.serve_forever()
+        print('\nServer Address: ftp://{}:{}\n'.format(address[0], address[1]))
+
+    def start_server(self):
+
+        # Launch FTP Server
+        self.server.serve_forever()
--- terminal.py
+++ terminal.py
@@ -2,137 +2,140 @@ import os
 import user
 import host
 
+
 class Terminal:
-	
-	user = None
-	host = None
-	login_msg = None
-	logout_msg = None
-	
-	def __init__(self):
-	    
-		self.user = user.User()
-		self.host = host.Host()
-	
-	# Shows welcome message
-	def display_startup_sequence(self):
-		
-		print('Python FTP Manager')
-		print('---------------------------------')
-		print('Version 0.8.3, by CodeArch')
-		print('\n')
-		
-	# Asks if the user wants to host a server or connect to an existing one
-	def ask_if_server(self):
-		
-		print('Choose an option from below: ')
-		print('\n')
-		print('[1]: Server')
-		print('[2]: Client')
-		print('\n')
-		print('Insert option: ', end='')
-		
-		user_option = input()
-		
-		if user_option == '1':
-			self.user.is_server = True
-		elif user_option == '2':
-			self.user.is_server = False
-		
-		return self.user.is_server
-		
-	def display_server_config_sequence(self):
-		
-		print('Insert username: ', end='')
-		self.user.username = input()
-		
-		print('Insert password: ', end='')
-		self.user.password = input()
-		
-		while True:
-			
-			try:
-				print('Insert directory to share: ', end='')
-				self.user.dir_ = input()
-				
-				# Checking if a directory exists. If false, raise NotADirectoryError
-				# If it exists, break the loop and continue operations
-				if os.path.isdir(self.user.dir_) == False:
-					raise NotADirectoryError from OSError
-				break
-			except NotADirectoryError:
-				print('Error: Not a directory or directory not found')
-	
-		
-		print('Insert level of access (R for read/RW for Read/Write): ', end='')
-		access_level = input().upper()
-		while True:
-		
-			if access_level == 'R':
-				
-				# Sets Read Only permissions
-				self.user.permissions = 'elr'
-				break
-			
-			elif access_level == 'RW':
-				
-				# Sets Read/Write permissions
-				self.user.permissions = 'elradfmwMT'
-				break
-			
-			else:
-				
-				# Checks if the user inserted something else and asks again
-				print('Use R for read and RW for Read/Write: ', end='')
-				access_level = input().upper()
-		
-		# Returns user account information to be used as hosting parameters
-		return self.user
-	
-	def display_client_config_sequence(self):
-		
-		print('Insert address (default = 0.0.0.0): ', end='')
-		address = input()
-		
-		print('Insert port (default = 2121): ', end='')
-		port = input()
-		
-		if address == '':
-			address = '0.0.0.0'
-		
-		if port == '':
-			port = 2121
-		
-		else:
-			# The statement below will malfunction if the user types a string.
-			# An exception needs to be added here
-			port = int(port)
-		
-		print('Insert username (default = None): ', end='')
-		username = input()
-		
-		print('Insert password (default = None): ', end='')
-		password = input()
-		
-		# Sets all the information necessary in the Host class and returns these values
-		self.host.address = address
-		self.host.port = port
-		self.host.username = username
-		self.host.password = password
-		
-		return self.host
-		
-	def display_help_message(self):
-		with open('help.txt', 'r') as help_file:
-			line_index = 0
-			for line in help_file:
-				if line_index <= 3:
-					line_index += 1
-				else:
-					print(line, end='')
-		
-	def get_user_input(self):
-			
-			command = input('> ')
-			
-			return command
+
+    user = None
+    host = None
+    login_msg = None
+    logout_msg = None
+
+    def __init__(self):
+
+        self.user = user.User()
+        self.host = host.Host()
+
+    # Shows welcome message
+    def display_startup_sequence(self):
+
+        print('Python FTP Manager')
+        print('---------------------------------')
+        print('Version 0.8.3, by CodeArch')
+        print('\n')
+
+    # Asks if the user wants to host a server or connect to an existing one
+    def ask_if_server(self):
+
+        print('Choose an option from below: ')
+        print('\n')
+        print('[1]: Server')
+        print('[2]: Client')
+        print('\n')
+        print('Insert option: ', end='')
+
+        user_option = input()
+
+        if user_option == '1':
+            self.user.is_server = True
+        elif user_option == '2':
+            self.user.is_server = False
+
+        return self.user.is_server
+
+    def display_server_config_sequence(self):
+
+        print('Insert username: ', end='')
+        self.user.username = input()
+
+        print('Insert password: ', end='')
+        self.user.password = input()
+
+        while True:
+
+            try:
+                print('Insert directory to share: ', end='')
+                self.user.dir_ = input()
+
+                # Checking if a directory exists. If false, raise
+                # NotADirectoryError. If it exists, break the loop and continue
+                # operations
+                if os.path.isdir(self.user.dir_) is False:
+                    raise NotADirectoryError from OSError
+                break
+            except NotADirectoryError:
+                print('Error: Not a directory or directory not found')
+
+        print('Insert level of access (R for read/RW for Read/Write): ',
+              end='')
+        access_level = input().upper()
+        while True:
+
+            if access_level == 'R':
+
+                # Sets Read Only permissions
+                self.user.permissions = 'elr'
+                break
+
+            elif access_level == 'RW':
+
+                # Sets Read/Write permissions
+                self.user.permissions = 'elradfmwMT'
+                break
+
+            else:
+
+                # Checks if the user inserted something else and asks again
+                print('Use R for read and RW for Read/Write: ', end='')
+                access_level = input().upper()
+
+        # Returns user account information to be used as hosting parameters
+        return self.user
+
+    def display_client_config_sequence(self):
+
+        print('Insert address (default = 0.0.0.0): ', end='')
+        address = input()
+
+        print('Insert port (default = 2121): ', end='')
+        port = input()
+
+        if address == '':
+            address = '0.0.0.0'
+
+        if port == '':
+            port = 2121
+
+        else:
+            # The statement below will malfunction if the user types a string.
+            # An exception needs to be added here
+            port = int(port)
+
+        print('Insert username (default = None): ', end='')
+        username = input()
+
+        print('Insert password (default = None): ', end='')
+        password = input()
+
+        # Sets all the information necessary in the Host class and returns
+        # these values
+        self.host.address = address
+        self.host.port = port
+        self.host.username = username
+        self.host.password = password
+
+        return self.host
+
+    def display_help_message(self):
+        with open('help.txt', 'r') as help_file:
+            line_index = 0
+            for line in help_file:
+                if line_index <= 3:
+                    line_index += 1
+                else:
+                    print(line, end='')
+
+    def get_user_input(self):
+
+        command = input('> ')
+
+        return command
