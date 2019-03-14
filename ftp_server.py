54c5ecf52f52b393546e879260a1bd9e49ff0450
Henrique Peixoto <henrique.peixoto1512@gmail.com>
2019-03-14 17:14:45 -0400

    Code refactoring, exception handling and documentation edits

--- ftp_server.py
+++ ftp_server.py
@@ -13,30 +13,22 @@ class FTPServer:
 
     def __init__(self, user):
 
-        # Instantiates a 'virtual' users manager
         authorizer = DummyAuthorizer()
-
-        # Adds a new user according to the following parameters:
-        # username, password, homedir, perm="", msg_login="", msg_quit=""
+        
         authorizer.add_user(
             user.username,
             user.password,
             user.dir_,
             perm=user.permissions)
-
-        # Instantiates the FTPHandler class
+        
         handler = FTPHandler
         handler.authorizer = authorizer
-
-        # Defines a banner (string) that shows when user connects
+        
         handler.banner = 'Welcome to a Python FTP Server!'
 
-        # Instantiates the server class and listen on address [Machine
-        # IPv4]:2121
         address = (str(ip_retriever.get_ip_address()), 2121)
         self.server = ThreadedFTPServer(address, handler)
 
-        # Limits the amount of possible connections
         self.server.max_cons = 10
         self.server.max_cons_per_ip = 5
 
@@ -44,5 +36,4 @@ class FTPServer:
 
     def start_server(self):
 
-        # Launch FTP Server
         self.server.serve_forever()
--- ip_retriever.py
+++ ip_retriever.py
@@ -3,25 +3,20 @@ import socket
 
 
 def get_ip_address():
-    # Checks if the machine is using Windows
+
     if (os.name == 'nt'):
 
-        # On Windows the command below works perfectly fine and returns the
-        # IPv4. Unix-based systems tend to return 127.0.0.1 which doesn't work
+        # On Windows the command below works. However, Unix based systems
+        # return 127.0.0.1
         return socket.gethostbyname(socket.gethostname())
 
     else:
 
-        # Creates a socket
+        # A workaround to the issue above was to connect to Google Public
+        # DNS Server and return the address used in the connection
         s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
-
-        # Connects to the Google Public DNS Server
         s.connect(('8.8.8.8', 80))
-
-        # Retrieves IPv4 and port information. The IP is on the index 0
         ip_addr = s.getsockname()[0]
-
-        # Closes the socket
         s.close()
 
         return ip_addr
